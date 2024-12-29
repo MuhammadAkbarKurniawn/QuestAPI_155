50ee0b93586aa2a23b458a4dfd826ad69d47b94f
MuhammadAkbarKurniawn <akbar1107kurniawan@gmail.com>
2024-12-29 15:18:50 +0700

    Membuat fungsi homeStatus pada class HomeView.kt

--- app/src/main/java/com/example/datasiswajaringan/ui/view/HomeView.kt
+++ app/src/main/java/com/example/datasiswajaringan/ui/view/HomeView.kt
@@ -1,5 +1,8 @@
 package com.example.datasiswajaringan.ui.view
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
@@ -8,14 +11,17 @@ import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.datasiswajaringan.ui.customwidget.CostumeTopAppBar
 import com.example.datasiswajaringan.ui.navigation.DestinasiNavigasi
+import com.example.datasiswajaringan.ui.viewmodel.HomeUiState
 import com.example.datasiswajaringan.ui.viewmodel.HomeViewModel
 
 object DestinasiHome : DestinasiNavigasi{
@@ -62,5 +68,40 @@ fun HomeScreen(
                 viewModel.getMhs()
             }
         )
+    }
+}
+
+@Composable
+fun HomeStatus(
+    homeUiState: HomeUiState,
+    retryAction: () -> Unit,
+    modifier: Modifier = Modifier,
+    onDeleteClick: (String) -> Unit = {},
+    onDetailClick: (String) -> Unit = {}
+) {
+    when (homeUiState) {
+        is HomeUiState.Loading -> OnLoading(modifier = modifier.fillMaxSize())
+        is HomeUiState.Success -> {
+            if (homeUiState.mahasiswa.isEmpty()) {
+                Box(
+                    modifier = modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text(text = "Tidak ada data Kontak")
+                }
+            } else {
+                MhsLayout(
+                    mahasiswa = homeUiState.mahasiswa,
+                    modifier = modifier.fillMaxWidth(),
+                    onDetailClick = { nim ->
+                        onDetailClick(nim)
+                    },
+                    onDeleteClick = {
+                        onDeleteClick(it)
+                    }
+                )
+            }
+        }
+        is HomeUiState.Error -> OnError(retryAction, modifier = modifier.fillMaxSize())
     }
 }
