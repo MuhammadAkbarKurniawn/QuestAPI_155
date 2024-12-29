5838cef44c66f5387d822b820b9234736697ce07
MuhammadAkbarKurniawn <akbar1107kurniawan@gmail.com>
2024-12-29 13:13:06 +0700

    Membuat class MahasiswaApplications

---
+++ app/src/main/java/com/example/datasiswajaringan/MahasiswaApplications.kt
@@ -0,0 +1,13 @@
+package com.example.datasiswajaringan
+
+import android.app.Application
+import com.example.datasiswajaringan.dependenciesinjection.AppContainer
+import com.example.datasiswajaringan.dependenciesinjection.MahasiswaContainer
+
+class MahasiswaApplications:Application {
+    lateinit var container: AppContainer
+    override fun onCreate(){
+        super.onCreate()
+        container = MahasiswaContainer
+    }
+}
