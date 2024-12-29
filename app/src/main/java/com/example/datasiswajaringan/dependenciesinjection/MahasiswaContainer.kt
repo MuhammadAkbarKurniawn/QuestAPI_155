a426db5497dfbd1db7082bad0301e98808ebfd30
MuhammadAkbarKurniawn <akbar1107kurniawan@gmail.com>
2024-12-29 13:08:59 +0700

    Membuat MahasiswaContainer

---
+++ app/src/main/java/com/example/datasiswajaringan/dependenciesinjection/MahasiswaContainer.kt
@@ -0,0 +1,33 @@
+package com.example.datasiswajaringan.dependenciesinjection
+
+import com.example.datasiswajaringan.repository.MahasiswaRepository
+import com.example.datasiswajaringan.repository.NetworkMahasiswaRepository
+import com.example.datasiswajaringan.service.MahasiswaService
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+
+interface AppContainer {
+    val kontakRepository: MahasiswaRepository
+}
+
+class MahasiswaContainer : AppContainer {
+
+    private val baseUrl = "http://10.0.2.2:8000/umyTI/" // localhost diganti IP jika dijalankan di perangkat lain
+    private val json = Json {
+        ignoreUnknownKeys = true
+    }
+    private val retrofit: Retrofit = Retrofit.Builder()
+        .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+        .baseUrl(baseUrl)
+        .build()
+
+    private val mahasiswaService: MahasiswaService by lazy {
+        retrofit.create(MahasiswaService::class.java)
+    }
+
+    override val kontakRepository: MahasiswaRepository by lazy {
+        NetworkMahasiswaRepository(mahasiswaService)
+    }
+}
