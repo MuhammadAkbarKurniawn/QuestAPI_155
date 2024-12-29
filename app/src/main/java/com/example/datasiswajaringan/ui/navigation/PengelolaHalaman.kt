b8607a3ff001a08e592e708e8301fb06bd0375ef
MuhammadAkbarKurniawn <akbar1107kurniawan@gmail.com>
2024-12-29 15:54:42 +0700

    Membuat fungsi untuk mengelola halaman

---
+++ app/src/main/java/com/example/datasiswajaringan/ui/navigation/PengelolaHalaman.kt
@@ -0,0 +1,39 @@
+package com.example.datasiswajaringan.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.datasiswajaringan.ui.view.DestinasiEntry
+import com.example.datasiswajaringan.ui.view.DestinasiHome
+import com.example.datasiswajaringan.ui.view.EntryMhsScreen
+import com.example.datasiswajaringan.ui.view.HomeScreen
+
+@Composable
+fun PengelolaHalaman(navController: NavHostController = rememberNavController()) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ) {
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = {navController.navigate(DestinasiEntry.route)},
+                onDetailClick = {
+
+                }
+            )
+        }
+        composable(DestinasiEntry.route) {
+            EntryMhsScreen(navigateBack = {
+                navController.navigate(DestinasiHome.route) {
+                    popUpTo(DestinasiHome.route){
+                    inclusive = true
+                    }
+                }
+            })
+        }
+    }
+}
