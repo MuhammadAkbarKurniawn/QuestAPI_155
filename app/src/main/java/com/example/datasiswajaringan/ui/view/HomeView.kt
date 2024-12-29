f08fa47b424849ced6bdc6c70f50df8cf8b26f24
MuhammadAkbarKurniawn <akbar1107kurniawan@gmail.com>
2024-12-29 15:29:40 +0700

    Membuat fungsi tampilan Mhs pada class HomeView.kt

--- app/src/main/java/com/example/datasiswajaringan/ui/view/HomeView.kt
+++ app/src/main/java/com/example/datasiswajaringan/ui/view/HomeView.kt
@@ -2,13 +2,16 @@ package com.example.datasiswajaringan.ui.view
 
 import android.media.Image
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Button
@@ -29,6 +32,7 @@ import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.datasiswajaringan.R
+import com.example.datasiswajaringan.model.Mahasiswa
 import com.example.datasiswajaringan.ui.customwidget.CostumeTopAppBar
 import com.example.datasiswajaringan.ui.navigation.DestinasiNavigasi
 import com.example.datasiswajaringan.ui.viewmodel.HomeUiState
@@ -143,6 +147,31 @@ fun OnError(retryAction: () -> Unit, modifier: Modifier = Modifier) {
         Text(text = stringResource(R.string.loading_failed), modifier = Modifier.padding(16.dp))
         Button(onClick = retryAction) {
             Text(stringResource(R.string.retry))
+        }
+    }
+}
+
+@Composable
+fun MhsLayout(
+    mahasiswa: List<Mahasiswa>,
+    modifier: Modifier = Modifier,
+    onDetailClick: (Mahasiswa) -> Unit,
+    onDeleteClick: (Mahasiswa) -> Unit = {}
+) {
+    LazyColumn(
+        modifier = modifier,
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        items(mahasiswa) { kontak ->
+            MhsCard(
+                mahasiswa = kontak,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable { onDetailClick(kontak) }
+            ) {
+                onDeleteClick(kontak)
+            }
         }
     }
 }
