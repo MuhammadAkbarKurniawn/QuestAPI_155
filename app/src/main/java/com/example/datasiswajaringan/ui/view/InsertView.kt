abd98846d264d85d8ad2cb8dac8845dd5db76242
MuhammadAkbarKurniawn <akbar1107kurniawan@gmail.com>
2024-12-29 14:08:36 +0700

    Membuat class InsertView.kt

---
+++ app/src/main/java/com/example/datasiswajaringan/ui/view/InsertView.kt
@@ -0,0 +1,31 @@
+package com.example.datasiswajaringan.ui.view
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.datasiswajaringan.ui.customwidget.CostumeTopAppBar
+import com.example.datasiswajaringan.ui.navigation.DestinasiNavigasi
+import com.example.datasiswajaringan.ui.viewmodel.InsertViewModel
+
+
+object DestinasiEntry : DestinasiNavigasi{
+    override val route = "item_entry"
+    override val titleRes = "Entry Mhs"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntryMhsScreen(
+    navigateBack:()-> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InsertViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+}
