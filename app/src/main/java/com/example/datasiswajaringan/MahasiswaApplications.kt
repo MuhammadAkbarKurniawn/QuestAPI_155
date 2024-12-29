ae38ee8d2c5ac6c31794bdc791d5fbfd85a0b98f
MuhammadAkbarKurniawn <akbar1107kurniawan@gmail.com>
2024-12-29 13:13:47 +0700

    Memperbaiki class MahasiswaApplications

--- app/src/main/java/com/example/datasiswajaringan/MahasiswaApplications.kt
+++ app/src/main/java/com/example/datasiswajaringan/MahasiswaApplications.kt
@@ -4,10 +4,10 @@ import android.app.Application
 import com.example.datasiswajaringan.dependenciesinjection.AppContainer
 import com.example.datasiswajaringan.dependenciesinjection.MahasiswaContainer
 
-class MahasiswaApplications:Application {
+class MahasiswaApplications:Application() {
     lateinit var container: AppContainer
     override fun onCreate(){
         super.onCreate()
-        container = MahasiswaContainer
+        container = MahasiswaContainer()
     }
 }
