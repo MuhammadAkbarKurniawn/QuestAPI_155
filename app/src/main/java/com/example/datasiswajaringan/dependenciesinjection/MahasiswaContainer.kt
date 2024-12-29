808c977a73bdacf8c16712d12a66d19dbf1016d8
MuhammadAkbarKurniawn <akbar1107kurniawan@gmail.com>
2024-12-29 18:53:32 +0700

    memperbaiki container error

--- app/src/main/java/com/example/datasiswajaringan/dependenciesinjection/MahasiswaContainer.kt
+++ app/src/main/java/com/example/datasiswajaringan/dependenciesinjection/MahasiswaContainer.kt
@@ -8,19 +8,23 @@ import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
 import retrofit2.Retrofit
 
-interface AppContainer{
+interface AppContainer {
     val kontakRepository: MahasiswaRepository
 }
 
-class MahasiswaContainer: AppContainer{
-    private val baseUrl = "http://10.0.2.2:8000/umyTI/"
-    private val json = Json { ignoreUnknownKeys = true }
+class MahasiswaContainer : AppContainer {
+
+    private val baseUrl = "http://10.0.2.2:8000/umyTI/" // localhost diganti IP jika dijalankan di perangkat lain
+    private val json = Json {
+        ignoreUnknownKeys = true
+    }
     private val retrofit: Retrofit = Retrofit.Builder()
         .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
-        .baseUrl(baseUrl).build()
+        .baseUrl(baseUrl)
+        .build()
 
     private val mahasiswaService: MahasiswaService by lazy {
-        retrofit.create(mahasiswaService::class.java)
+        retrofit.create(MahasiswaService::class.java)
     }
 
     override val kontakRepository: MahasiswaRepository by lazy {
