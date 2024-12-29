a5018ee29ca5e5ff19025a8a44b87e001f05cdf8
MuhammadAkbarKurniawn <akbar1107kurniawan@gmail.com>
2024-12-29 15:25:49 +0700

    Membuat fungsi OnLoading dan On Error pada class HomeView.kt

--- app/src/main/java/com/example/datasiswajaringan/ui/view/HomeView.kt
+++ app/src/main/java/com/example/datasiswajaringan/ui/view/HomeView.kt
@@ -1,11 +1,17 @@
 package com.example.datasiswajaringan.ui.view
 
+import android.media.Image
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -17,8 +23,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.datasiswajaringan.R
 import com.example.datasiswajaringan.ui.customwidget.CostumeTopAppBar
 import com.example.datasiswajaringan.ui.navigation.DestinasiNavigasi
 import com.example.datasiswajaringan.ui.viewmodel.HomeUiState
@@ -103,5 +113,36 @@ fun HomeStatus(
             }
         }
         is HomeUiState.Error -> OnError(retryAction, modifier = modifier.fillMaxSize())
+    }
+}
+
+//the homescreen displaying the loading message
+
+@Composable
+fun OnLoading(modifier: Modifier = Modifier){
+    Image(
+        modifier = modifier.size(200.dp),
+        painter = painterResource(R.drawable.loading),
+        contentDescription = stringResource(R.string.loading)
+    )
+}
+
+//the homescreen displaying error message with re-attempt button
+
+@Composable
+fun OnError(retryAction: () -> Unit, modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.connection_error),
+            contentDescription = ""
+        )
+        Text(text = stringResource(R.string.loading_failed), modifier = Modifier.padding(16.dp))
+        Button(onClick = retryAction) {
+            Text(stringResource(R.string.retry))
+        }
     }
 }
